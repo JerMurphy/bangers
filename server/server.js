626f190340945b9c274d85b27b7b13a9573dc9f4
Jeremy Murphy <jeremy@massively.ai>
2019-02-15 16:24:46 -0500

    commiting spotify integration

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const cors          = require('cors');
+const rp            = require('request-promise'); 
+const SpotifyWebApi = require('spotify-web-api-node');
+const config        = require('./config.js');
 const app = express();
 //Middleware
 app.use(bodyParser.json());
@@ -9,8 +12,14 @@ app.use(cors());
 
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
 const _ = require('lodash');
+
+// The API object we'll use to interact with the API
+var spotifyApi = new SpotifyWebApi({
+  clientId : config.spotify.clientId,
+  clientSecret : config.spotify.clientSecret,
+  redirectUri : config.spotify.redirectUri
+});
 
 var fresh_list = [
     {
@@ -39,32 +48,74 @@ var fresh_list = [
     }
   ];
 
+//playlist socket integration
 io.on('connection', function (socket) {
-    console.log('socket connected to room:', socket.handshake.query.room)
-    socket.join(socket.handshake.query.room);
-    io.in(socket.handshake.query.room).emit('fresh_list', fresh_list)
-    socket.on('new_song', function (data) {
-        fresh_list.push(data); // for now im pushing to a variable, will add a room specific db later 
-        io.in(socket.handshake.query.room).emit('new_song', data)
-    });
-    socket.on('plus_minus', function (data) {
-        if(data.dir == 'up'){
-            fresh_list[data.index].count++;
-        }else{
-            fresh_list[data.index].count--;
-        }
-        if(fresh_list[data.index].count <= 0){
-            fresh_list.splice(data.index,1)
-        }
-        fresh_list = _.orderBy(fresh_list, ['count'],['desc']); 
+  console.log('socket connected to room:', socket.handshake.query.room)
+  socket.join(socket.handshake.query.room);
+  io.in(socket.handshake.query.room).emit('fresh_list', fresh_list)
+  socket.on('new_song', function (data) {
+      fresh_list.push(data); // for now im pushing to a variable, will add a room specific db later 
+      io.in(socket.handshake.query.room).emit('new_song', data)
+  });
+  socket.on('plus_minus', function (data) {
+      if(data.dir == 'up'){
+          fresh_list[data.index].count++;
+      }else{
+          fresh_list[data.index].count--;
+      }
+      if(fresh_list[data.index].count <= 0){
+          fresh_list.splice(data.index,1)
+      }
+      fresh_list = _.orderBy(fresh_list, ['count'],['desc']); 
+      // for now im pushing to a variable, will add a room specific db later 
+        io.in(socket.handshake.query.room).emit('plus_minus', fresh_list)
+  });
+  socket.on('delete_song', function (data) {
+      fresh_list.splice(data,1)
         // for now im pushing to a variable, will add a room specific db later 
-         io.in(socket.handshake.query.room).emit('plus_minus', fresh_list)
-    });
-    socket.on('delete_song', function (data) {
-        fresh_list.splice(data,1)
-         // for now im pushing to a variable, will add a room specific db later 
-         io.in(socket.handshake.query.room).emit('delete_song', data)
-    });
+        io.in(socket.handshake.query.room).emit('delete_song', data)
+  });
+});
+
+app.get('/login', function(req, res) {
+  console.log('login hit')
+  var scopes = ['user-read-private', 'user-read-email'];
+  var authorizeURL = spotifyApi.createAuthorizeURL(scopes, null, true);
+  res.send(authorizeURL);
+});
+
+app.get('/login_redirect', function(req,res){
+  var authorizationCode = req.query.code;
+  var access_token;
+  var refresh_token;
+  spotifyApi.authorizationCodeGrant(authorizationCode)
+  .then(function(data) {
+    access_token = data.body.access_token;
+    refresh_token = data.body.refresh_token;
+    var options = {
+      method: 'GET',
+      uri: 'https://api.spotify.com/v1/me',
+      headers: {
+        "Authorization": "Bearer " + data.body.access_token
+      }
+    }
+    return rp(options).then(function(resp){
+      resp = JSON.parse(resp);
+      var obj = {};
+      obj.access_token = access_token;
+      obj.refresh_token = refresh_token;
+      obj.name = resp.display_name;
+      //obj.picture = resp.images[0].url.split('//')[1];
+      send_obj = JSON.stringify(obj);
+      console.log(send_obj);
+      res.redirect('http://localhost:8080/#/join?user_data='+ send_obj);
+    }).catch(function(err){
+      console.log(err)
+      res.send(err);
+    })
+  }, function(err) {
+    console.log('Something went wrong when retrieving the access token!', err.message);
+  });
 });
 
 
